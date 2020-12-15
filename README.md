b1c3554e5a71e5e9327a5717dc7c3af94eb66975
Haim Adrian <Haim.Adrian@quest.com>
2020-12-15 03:36:10 +0200

    Fix links to be relative, rather than localhost, and publish application to GitHub Pages

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MathematicalTools
 Mathematical Tools Calculator with fully detailed answer. Can be used to solve home work, etc.
+
+# App
+[Click Here to go to the application](https://haimadrian.github.io/MathematicalTools/)
--- math-tools-calc/src/App.js
+++ math-tools-calc/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Router, Route, Switch, Redirect} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import history from './common/history';
 import EuclideanAlgorithmForm from './view/actions/euclidean-algorithm/index';
 import GcdLcmForm from './view/actions/gcd-lcm-prime-factors/index';
@@ -9,35 +9,25 @@ import ModularEquationsForm from './view/actions/modular-equations/index';
 function App() {
     return (
         <div className="App">
-            <Router history={history}>
+            <header className="App-header">
+                <img src="./deadpool.png" className="App-logo" alt="logo"/>
+            </header>
+            <BrowserRouter history={history}>
                 <Switch>
                     <Route exact path='/euclideanalgorithm' component={EuclideanAlgorithmForm}/>
                     <Route exact path='/gcdlcm' component={GcdLcmForm}/>
                     <Route exact path='/euler' component={EulerForm}/>
                     <Route exact path='/modularequations' component={ModularEquationsForm}/>
                 </Switch>
-            </Router>
-            <header className="App-header">
-                <img src="./deadpool.png" className="App-logo" alt="logo"/>
-            </header>
-            <body className="App-body">
-                <a className="App-link" href="http://localhost:3000/" rel="noopener noreferrer">
-                    Home
-                </a>
-                <a className="App-link" href="http://localhost:3000/euclideanalgorithm" rel="noopener noreferrer">
-                    Extended Euclid's Algorithm (Bezout)
-                </a>
-                <a className="App-link" href="http://localhost:3000/gcdlcm" rel="noopener noreferrer">
-                    GCD, LCM and Prime Factors
-                </a>
-                <a className="App-link" href="http://localhost:3000/euler" rel="noopener noreferrer">
-                    Euler (Function and Formula) and Gauss Sum
-                </a>
-                <a className="App-link" href="http://localhost:3000/modularequations" rel="noopener noreferrer">
-                    Modular Equations
-                </a>
-                <label className="copyright">© All Rights ... wtf lol © RickShvetz © 2020-2021 ©</label>
-            </body>
+                <body className="App-body">
+                    <Link to="/" className="App-link">Home</Link>
+                    <Link to="/euclideanalgorithm" className="App-link">Extended Euclid's Algorithm (Bezout)</Link>
+                    <Link to="/gcdlcm" className="App-link">GCD, LCM and Prime Factors</Link>
+                    <Link to="/euler" className="App-link">Euler (Function and Formula) and Gauss Sum</Link>
+                    <Link to="/modularequations" className="App-link">Modular Equations</Link>
+                    <label className="copyright">© All Rights ... wtf lol © RickShvetz © 2020-2021 ©</label>
+                </body>
+            </BrowserRouter>
         </div>
     );
 }
